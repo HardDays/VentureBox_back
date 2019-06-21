631cb6f7a79de6fa1c7572d76c861ff975efc4d0
aalbina <aalmukhametova@gmail.com>
2019-06-21 21:11:18 +0300

    encode score url

--- app/helpers/signum_exchange.rb
+++ app/helpers/signum_exchange.rb
@@ -1,8 +1,12 @@
+require "erb"
+include ERB::Util
+
 class SignumExchange
   include HTTParty
   base_uri 'scout.tenoris.net'
 
   def get_index(company_domain)
+    company_domain = url_encode(company_domain)
     result = self.class.get("/index-output.php?domain=#{company_domain}")
 
     unless result.code == 200
